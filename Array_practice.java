b94df257ba66c4cd89334a69f0fee5930d9ed22d
ravikanth995 <ravikanthchauhan95@gmail.com>
2024-01-27 14:26:12 +0530

    Java Arrays

---
+++ Array_practice.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class Array_practice {
+    public static void main(String[] args) {
+        // int a []= new int [5];
+        // a[0] = 1;
+        // a[1] = 2;
+        // a[2] = 3;
+        // a[3] = 4;
+        // a[4] = 5;
+
+        // System.out.println(Arrays.toString(a));
+
+        // int a [] = {10, 20, 30, 40, 50};
+        // for (int x:a)
+        // {
+        //     System.err.println(x);
+        // }
+        
+        // int a [] = {10, 20, 30, 40, 50};
+        // System.out.println(a.length);
+        // for (int x:a)
+        // {
+        //     System.out.println(x);
+        // }
+        // int a [] = {10, 20, 30, 40, 50, 60};
+        // System.err.println(a.length);
+        // for (int i:a)
+        // {
+        //     System.out.println(i);
+        // }
+
+        int a [] = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(a.length);
+        for (int i:a)
+        {
+           System.out.println(i);
+        }
+        
+    }
+}
